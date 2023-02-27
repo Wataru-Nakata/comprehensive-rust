90f46a638d1d880c496b7f37828f58067bcd1bb6
Wataru Nakata <wataru9871@gmail.com>
2023-02-28 00:45:39 +0900

    added web server

---
+++ src/bin/web-server.rs
@@ -0,0 +1,52 @@
+// TODO: remove this when you're done with your implementation.
+#![allow(unused_variables, dead_code)]
+
+
+pub fn prefix_matches(prefix: &str, request_path: &str) -> bool {
+    let prefix_iter = prefix.split('/');
+    let request_path_iter = request_path.split('/');
+    if prefix_iter.clone().count() > request_path_iter.clone().count(){
+        return false;
+    }
+    for (a, b) in prefix_iter.zip(request_path_iter)
+    {
+        if a != b && !a.contains('*') {
+            return false;
+        }
+
+    }
+    return true;
+}
+
+#[test]
+fn test_matches_without_wildcard() {
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc-123"));
+    assert!(prefix_matches("/v1/publishers", "/v1/publishers/abc/books"));
+
+    assert!(!prefix_matches("/v1/publishers", "/v1"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/publishersBooks"));
+    assert!(!prefix_matches("/v1/publishers", "/v1/parent/publishers"));
+}
+
+#[test]
+fn test_matches_with_wildcard() {
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/bar/books"
+    ));
+    assert!(prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/books/book1"
+    ));
+
+    assert!(!prefix_matches("/v1/publishers/*/books", "/v1/publishers"));
+    assert!(!prefix_matches(
+        "/v1/publishers/*/books",
+        "/v1/publishers/foo/booksByAuthor"
+    ));
+}
